3ac7d2da0cff6635bf2dcb8ba6c0db6a2c3ae505
anbuchezhiyan2005 <153158019+anbuchezhiyan2005@users.noreply.github.com>
2024-01-26 20:28:26 +0530

    Create hackerearth.c

---
+++ hackerearth.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+void ans();
+
+int main(){
+	
+	ans();
+	return 0;
+}
+
+void ans(){
+	int num;
+	while(1){
+		scanf("%d",&num);
+		if(num == 42){
+			break;
+		}
+		else{
+			printf("%d\n",num);
+		}
+	}
+}
